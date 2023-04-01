ea90f05b650c0df3261b0f9c6d4b4df041bccfd3
Kyle Lau <kyle.lau15@gmail.com>
2023-04-01 15:40:52 -0400

    edits to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,11 +10,6 @@ const Form = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         //   Start
-            // const params = new URLSearchParams();
-            // params.append("limit", 10);
-            // params.append("offset", 0);
-            // params.append("rating", "pg");
-            // params.append("lang", "en");
         
         axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&limit=10&offset=0&rating=pg&lang=en`)
             .then(response => {
