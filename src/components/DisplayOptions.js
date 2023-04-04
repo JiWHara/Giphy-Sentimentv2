3fd6080d57bfecdbb6f4bf47b4e0356d86eeee0b
Jackson Watt-Bowers <wattbowers@gmail.com>
2023-04-04 12:47:37 -0400

    modal

--- src/components/DisplayOptions.js
+++ src/components/DisplayOptions.js
@@ -6,17 +6,24 @@ import { useState, useEffect } from "react";
 // DisplayOptions function
 const DisplayOptions = ({ gifArray }) => {
 
+    const [showModal, setShowModal] = useState('')
+    const database = getDatabase(firebaseInfo)
+    // reference to db
+    const dbRef = ref(database) 
+    
+    
+
+    const clickHandler = (dbRef, gifObj) => {
+        push(dbRef, gifObj)
+    }
+
     return (
         <ul className='gifList'>
                 {gifArray ?
                     gifArray.map(gif => {
-                        const database = getDatabase(firebaseInfo)
-                        // reference to db
-                        const dbRef = ref(database) 
                         
                         const timeElapsed = Date.now();
                         const currentDay = new Date(timeElapsed)
-
                         const gifObj = {
                             img: gif.images.original.url,
                             alt: gif.title,
@@ -24,14 +31,23 @@ const DisplayOptions = ({ gifArray }) => {
                             time: currentDay.toDateString(),
                             timeNum: timeElapsed
                         }
-
-                        const clickHandler = () => {
-                            push(dbRef, gifObj)
-                        }
+                        
+                        
                         return (
-                            <li className="gifContainer" key={gif.id} onClick={clickHandler}>
-                                <img src={gif.images.original.url} alt={gif.title} />
-                            </li>
+                            <div key={gif.id}>
+                                <li className="gifContainer" onClick={() => setShowModal(`${gif.title}`)}>
+                                    <img src={gif.images.original.url} alt={gif.title} />
+                                </li>
+                                <div id="wbwModal" className={`${ showModal === gif.title ? `modal` : `hidden` }`}>
+                                    <div className="modalFlex">
+                                        <button onClick={() => setShowModal('')} className="btnClose">X</button>
+                                        <p><strong>{gif.title}</strong> is a comprehensive movie tracking application that enables you to easily manage your to-watch list.   The platform was built using a combination of <span>Javascript</span>, <span>HTML</span>, <span>CSS</span>, <span>Sass</span>, and    <span>Firebase</span> for the backend, ensuring a highly responsive and efficient user experience. The site is hosted on<span>Netlify</span>.</p>
+
+                                        <p>Our platform includes a user authentication system that enables each user to keep track of their own list of movies. This feature    ensures that every user's preferences are catered to, allowing them to tailor their movie tracking experience to their needs.</p>
+                                    </div>
+                                </div>
+                                <div id="wbwOverlay" class={`${ showModal === gif.title ? `overlay` : `hidden` }`}></div>
+                            </div>
                         )
                     })
                     :
